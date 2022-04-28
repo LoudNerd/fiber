f3a3030eeb5854f0d4a10cf9bf454110f4491974
LoudNerd <techwizzy13@gmail.com>
2022-04-28 23:48:19 +0300

    initial commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-navigation-drawer app>
+    <v-img
+      height="80"
+
+      class="pa-4"
+      src="https://www.fiberlinklimited.co.ke/wp-content/uploads/2022/01/logo-5.png"
+    >
+    
+    <v-spacer> </v-spacer>
+    
+      <div class="text-center">
+        <v-avatar class="mb-4" color="grey darken-1" size="64">
+          <v-img
+            aspect-ratio="30"
+            src="https://www.fiberlinklimited.co.ke/wp-content/uploads/2022/01/logo-5.png"
+          />
+        </v-avatar>
+        <h2 class="white--text">FiberLink</h2>
+      </div>
+
+    </v-img>
+    <v-divider></v-divider>
+
+    <v-list>
+        <v-spacer> </v-spacer>
+      <v-list-item v-for="[icon, text] in links" :key="icon" link>
+        <v-list-item-icon>
+          <v-icon>{{ icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ text }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+ // eslint-disable-next-line vue/multi-word-component-names
+ name: "Sidebar",
+ props : ["drawer"],
+  data() {
+    return {
+      links: [
+        ["mdi-microsoft-windows", "Dashboard"],
+        ["mdi-account", "Profile"],
+        ["mdi-clipboard-list-outline", "Products"],
+        ["mdi-card-account-details-outline", "Orders"],
+        ["mdi-cog", "System Setting"],
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import router from './router'
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC9HrAb3JjGe5XdbYo79Mx_hNSDUSWngiU",
+//   authDomain: "dashboard-7ce85.firebaseapp.com",
+//   projectId: "dashboard-7ce85",
+//   storageBucket: "dashboard-7ce85.appspot.com",
+//   messagingSenderId: "581619969569",
+//   appId: "1:581619969569:web:ace3c8fa983fa0b1227d60",
+//   measurementId: "G-LYFSPBDM4C"
+// }
+
+// firebase.initializeApp(firebaseConfig)
+
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
